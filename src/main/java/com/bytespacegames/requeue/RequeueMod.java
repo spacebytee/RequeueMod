6e523de746c3424dbe336d5087ae6a63e885c5bd
space <55336451+spacebytee@users.noreply.github.com>
2025-01-14 00:38:07 -0500

    change version id

--- src/main/java/com/bytespacegames/requeue/RequeueMod.java
+++ src/main/java/com/bytespacegames/requeue/RequeueMod.java
@@ -15,7 +15,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 @Mod(modid = RequeueMod.MODID, version = RequeueMod.VERSION)
 public class RequeueMod {
     public static final String MODID = "requeuemod";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.1";
     public static final String MOD_PREFIX = "§cspace's requeue";
     public static RequeueMod instance;
 
